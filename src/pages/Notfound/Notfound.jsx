0c68e626d25c1c437c290fa6739c5a1eea64dd58
Yongmin0423 <yym21345@gmail.com>
2025-04-19 01:08:53 +0900

    first commit

---
+++ src/pages/Notfound/Notfound.jsx
@@ -0,0 +1,3 @@
+export default function Notfound() {
+  return <h1>Notfound</h1>;
+}
---
+++ src/router.jsx
@@ -0,0 +1,35 @@
+import { createBrowserRouter } from "react-router-dom";
+import AppLayout from "./AppLayout";
+import Home from "./pages/Home/Home";
+import Movies from "./pages/Movies/Movies";
+import MovieDetail from "./pages/MovieDetail/MovieDetail";
+import Notfound from "./pages/Notfound/Notfound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "movies",
+        element: <Movies />,
+        children: [
+          {
+            path: ":id",
+            element: <MovieDetail />,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <Notfound />,
+  },
+]);
+
+export default router;
